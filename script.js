ddeccbe79f1aafda911412ae0167f3ee7a5eb9c1
DavidHamelin <hamelindavid@hotmail.fr>
2019-01-11 10:05:06 +0100

    js Ex 06 regexNum included

---
+++ script.js
@@ -0,0 +1,18 @@
+﻿function modulo()
+{
+    var firstNumber = document.getElementById('firstNumber').value;
+    var secondNumber = document.getElementById('secondNumber').value;
+    var result = parseInt(firstNumber) / secondNumber;
+    var moduloResult = parseInt(firstNumber) % secondNumber;
+
+    var regexNum = /^[0-9.,]+$/i; /* n'accepte que les nombres */
+    if (regexNum.test(firstNumber) == false) {
+        alert('Premier nombre non valide')
+    }
+    else if (regexNum.test(secondNumber) == false) {
+        alert('Deuxième nombre non valide')
+    }
+    else {
+        alert(` ${firstNumber} / ${secondNumber} \= ${result} \nIl reste : ${moduloResult}`);
+    }
+}
